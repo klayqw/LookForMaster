6b92c4c01ab66f253f349e0d6e6da37f7ce03c13
Сикачевский Тимур Вадим <Sikac_kc43@itstep.edu.az>
2023-07-08 19:53:21 +0400

    Add design on SignUp Menu

--- LookForMaster/Views/SignUpPage.xaml.cs
+++ LookForMaster/Views/SignUpPage.xaml.cs
@@ -9,20 +9,36 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LookForMaster.Views
+namespace LookForMaster.Views;
+
+public partial class SignUpPage : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SignUpPage.xaml
-    /// </summary>
-    public partial class SignUpPage : UserControl
+    public SignUpPage()
     {
-        public SignUpPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void SigUpButton_MouseEnter(object sender, MouseEventArgs e)
+    {
+        DropShadowEffect dropShadowEffect = new DropShadowEffect();
+        dropShadowEffect.Color = Colors.LawnGreen;
+        dropShadowEffect.Direction = 0;
+        dropShadowEffect.ShadowDepth = 0;
+        dropShadowEffect.BlurRadius = 10;
+        SignUpButton.Effect = dropShadowEffect;
+    }
+    private void SigUpButton_MouseLeave(object sender, MouseEventArgs e)
+    {
+        DropShadowEffect dropShadowEffect = new DropShadowEffect();
+        dropShadowEffect.Color = Colors.LawnGreen;
+        dropShadowEffect.Direction = 0;
+        dropShadowEffect.ShadowDepth = 0;
+        dropShadowEffect.BlurRadius = 5;
+        SignUpButton.Effect = dropShadowEffect;
     }
 }
