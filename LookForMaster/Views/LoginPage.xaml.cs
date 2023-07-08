054020f575502f71633f420875818f7e88372eb9
Сикачевский Тимур Вадим <Sikac_kc43@itstep.edu.az>
2023-07-08 19:28:30 +0400

    Add Animation On Buttons

--- LookForMaster/Views/LoginPage.xaml.cs
+++ LookForMaster/Views/LoginPage.xaml.cs
@@ -9,20 +9,43 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LookForMaster.Views
+namespace LookForMaster.Views;
+public partial class LoginPage : UserControl
 {
-    /// <summary>
-    /// Interaction logic for LoginPage.xaml
-    /// </summary>
-    public partial class LoginPage : UserControl
+    public bool IsMaxsimize = false;
+    public LoginPage()
     {
-        public LoginPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+    private void SignUpButton_MouseLeave(object sender, MouseEventArgs e) => SignUpButton.Content = "Sign Up";
+    private void SignUpButton_MouseEnter(object sender, MouseEventArgs e)
+    {
+        SignUpButton.Content = "Sign Up";
+        TextBlock textBlock = new TextBlock(new Run(SignUpButton.Content.ToString()));
+        textBlock.TextDecorations = TextDecorations.Underline;
+        SignUpButton.Content = textBlock;
+    }
+    private void LoginButton_MouseEnter(object sender, MouseEventArgs e)
+    {
+        DropShadowEffect dropShadowEffect = new DropShadowEffect();
+        dropShadowEffect.Color = Colors.LawnGreen;
+        dropShadowEffect.Direction = 0;
+        dropShadowEffect.ShadowDepth = 0;
+        dropShadowEffect.BlurRadius = 10;
+        LoginButton.Effect = dropShadowEffect;
+    }
+    private void LoginButton_MouseLeave(object sender, MouseEventArgs e)
+    {
+        DropShadowEffect dropShadowEffect = new DropShadowEffect();
+        dropShadowEffect.Color = Colors.LawnGreen;
+        dropShadowEffect.Direction = 0;
+        dropShadowEffect.ShadowDepth = 0;
+        dropShadowEffect.BlurRadius = 5;
+        LoginButton.Effect = dropShadowEffect;
     }
 }
